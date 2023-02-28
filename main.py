c80e9a313bd289d61bc3790cbc423ee9867925a9
musakovkirill <mysakov_kirill@mail.ru>
2023-03-01 01:46:42 +0500

    усиленно делаю калькулятор

--- main.py
+++ main.py
@@ -1,15 +1,42 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton, \
+    QLineEdit, QSizePolicy
 
+class Calculadora(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Calculadora do Vitor')
+        self.setFixedSize(600, 600)
+        self.cw = QWidget()
+        self.grid = QGridLayout(self.cw)
 
+        self.display = QLineEdit()
+        self.grid.addWidget(self.display, 0, 0, 1, 5)
+        self.display.setDisabled(True)
+        self.display.setStyleSheet(
+            '* {background: #FFF; color #000; font-size: 35px;}'
+        )
+        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
 
+        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
+        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
+        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
+        self.add_btn(QPushButton('+'), 1, 3, 1, 1)
+        self.add_btn(
+            QPushButton('C'), 1, 4, 1, 1,
+            lambda: self.display.setText(''),
+            '* {background: orange; color: #fff ;font-weight: 700;}'
 
-app = QApplication(sys.argv)
+        )
 
-w = QWidget()
-w.resize(1080, 720)
-w.move(120, 120)
-w.setWindowTitle('Tesla charger map')
-w.show()
-
-sys.exit(app.exec_())
+        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
+        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
+        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
+        self.add_btn(QPushButton('-'), 2, 3, 1, 1)
+        self.add_btn(
+            QPushButton('<-'), 2, 4, 1, 1,
+            lambda: self.display.setText((
+                self.display.text()[:-1]
+            )),
+            '* {background: green; color: #fff ; font-weight: 700;}'
+        )
