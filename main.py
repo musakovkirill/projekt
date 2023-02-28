af4a7145d794130135916761c0f73808eac6b23e
musakovkirill <mysakov_kirill@mail.ru>
2023-03-01 01:56:19 +0500

    process codinga

--- main.py
+++ main.py
@@ -1,42 +1,56 @@
+
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QPushButton, \
-    QLineEdit, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout, QLineEdit
+from PyQt5.QtCore import Qt
 
-class Calculadora(QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle('Calculadora do Vitor')
-        self.setFixedSize(600, 600)
-        self.cw = QWidget()
-        self.grid = QGridLayout(self.cw)
 
-        self.display = QLineEdit()
-        self.grid.addWidget(self.display, 0, 0, 1, 5)
-        self.display.setDisabled(True)
-        self.display.setStyleSheet(
-            '* {background: #FFF; color #000; font-size: 35px;}'
-        )
-        self.display.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+class Calculator(QWidget):
+    def __init__(self):
+        super().__init__()
 
-        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
-        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
-        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
-        self.add_btn(QPushButton('+'), 1, 3, 1, 1)
-        self.add_btn(
-            QPushButton('C'), 1, 4, 1, 1,
-            lambda: self.display.setText(''),
-            '* {background: orange; color: #fff ;font-weight: 700;}'
+        self.initUI()
 
-        )
+    def initUI(self):
+        self.line = QLineEdit()
+        self.line.setReadOnly(True)
+        self.line.setAlignment(Qt.AlignRight)
+        self.line.setMaxLength(15)
 
-        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
-        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
-        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
-        self.add_btn(QPushButton('-'), 2, 3, 1, 1)
-        self.add_btn(
-            QPushButton('<-'), 2, 4, 1, 1,
-            lambda: self.display.setText((
-                self.display.text()[:-1]
-            )),
-            '* {background: green; color: #fff ; font-weight: 700;}'
-        )
+        self.btn_0 = QPushButton("0")
+        self.btn_1 = QPushButton("1")
+        self.btn_2 = QPushButton("2")
+        self.btn_3 = QPushButton("3")
+        self.btn_4 = QPushButton("4")
+        self.btn_5 = QPushButton("5")
+        self.btn_6 = QPushButton("6")
+        self.btn_7 = QPushButton("7")
+        self.btn_8 = QPushButton("8")
+        self.btn_9 = QPushButton("9")
+        self.btn_decimal = QPushButton(".")
+        self.btn_clear = QPushButton("C")
+        self.btn_add = QPushButton("+")
+        self.btn_subtract = QPushButton("-")
+        self.btn_multiply = QPushButton("*")
+        self.btn_divide = QPushButton("/")
+        self.btn_equals = QPushButton("=")
+
+        # Создание сетки для размещения кнопок
+        grid = QGridLayout()
+        grid.addWidget(self.line, 0, 0, 1, 4)
+        grid.addWidget(self.btn_1, 1, 0)
+        grid.addWidget(self.btn_2, 1, 1)
+        grid.addWidget(self.btn_3, 1, 2)
+        grid.addWidget(self.btn_add, 1, 3)
+        grid.addWidget(self.btn_4, 2, 0)
+        grid.addWidget(self.btn_5, 2, 1)
+        grid.addWidget(self.btn_6, 2, 2)
+        grid.addWidget(self.btn_subtract, 2, 3)
+        grid.addWidget(self.btn_7, 3, 0)
+        grid.addWidget(self.btn_8, 3, 1)
+        grid.addWidget(self.btn_9, 3, 2)
+        grid.addWidget(self.btn_multiply, 3, 3)
+        grid.addWidget(self.btn_decimal, 4, 0)
+        grid.addWidget(self.btn_0, 4, 1)
+        grid.addWidget(self.btn_clear, 4, 2)
+        grid.addWidget(self.btn_divide, 4, 3)
+        grid.addWidget(self.btn_equals, 5, 0, 1, 4)
